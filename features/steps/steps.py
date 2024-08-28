a88c7810f246929971a4d93c67c310e7cfbcf387
Daniel Eder <daniel@deder.at>
2024-08-28 12:06:50 +0200

    fix code smell "Dependent on Class State"

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -12,12 +12,6 @@ def step_impl(context):
     pass
 
 
-@given(u'the customer name is (?P<first_name>.*?) (?P<last_name>.*?)')
-def step_impl(context, first_name, last_name):
-    context.first_name = first_name
-    context.last_name = last_name
-
-
 @when(u'all customers are searched')
 def step_impl(context):
     context.count = len(context.service.search_customers())
@@ -34,30 +28,11 @@ def step_impl(context):
     assert_that(str(context.error)).is_equal_to("Mandatory name parameter is missing")
 
 
-
-@given(u'the second customer is (?P<first_name>.*?) (?P<last_name>.*?)')
+@then(u'the creation of customer (?P<first_name>.*?) (?P<last_name>.*?) should fail')
 def step_impl(context, first_name, last_name):
-    context.second_first_name = first_name
-    context.second_last_name = last_name
-
-
-@when(u'the second customer is created')
-def step_impl(context):
-    pass
-
-
-@then(u'the second customer can be found')
-def step_impl(context):
-    context.service.add_customer(context.second_first_name, context.second_last_name, context.default_birthday)
-    customer = context.service.search_customers(context.second_first_name, context.second_last_name)
-
-    assert_that(customer).is_not_none()
-
-@then(u'the second customer creation should fail')
-def step_impl(context):
     caught = None
     try:
-        context.service.add_customer(context.second_first_name, context.second_last_name, context.default_birthday)
+        context.service.add_customer(first_name, last_name, context.default_birthday)
     except ValueError as e:
         caught = e
 
@@ -71,12 +46,12 @@ def step_impl(context, first_name, last_name):
     assert_that(customer.last_name).is_equal_to(last_name)
 
 
-@given(u'the customer is created')
-@when(u'an invalid customer is created')
-@when(u'the customer is created')
-def step_impl(context):
+@when("the customer (?P<first_name>.*?) (?P<last_name>.*?) is created")
+@when("the second customer (?P<first_name>.*?) (?P<last_name>.*?) is created")
+@when("an invalid customer (?P<first_name>.*?) (?P<last_name>.*?) is created")
+def step_impl(context, first_name, last_name):
     try:
-        context.service.add_customer(context.first_name, context.last_name, context.default_birthday)
+        context.service.add_customer(first_name, last_name, context.default_birthday)
     except ValueError as e:
         context.error = e
 
